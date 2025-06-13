87a1a15b92939bcfc1588d550fefd8a97a5626b4
Ben Bronner <crow@black-bird-design.de>
2025-06-13 08:57:33 +0200

    added commits in loadContent Script

--- scripts/loadContent.js
+++ scripts/loadContent.js
@@ -1,10 +1,12 @@
+// Dieses Script lädt dynamisch den Header, Footer und den Content in die index.html und tauscht den Content dynamisch aus!
 class ContentLoader {
     constructor() {
         this.headerElement = document.getElementById('header');
         this.contentElement = document.getElementById('content');
         this.footerElement = document.getElementById('footer');
     }
-
+    
+    // Lädt den Content oder gibt eine Fehlermeldung aus wenn was nicht geladen werden kann
     async loadContent(url) {
         try {
             const response = await fetch(url);
@@ -18,23 +20,26 @@ class ContentLoader {
         }
     }
 
+    // Lädt den Header
     async loadHeader() {
         const headerContent = await this.loadContent('header.html');
         this.headerElement.innerHTML = headerContent;
     }
 
+    // Lädt die URL über das Menu bzw. klick auf die Bilder
     async loadPage(pageUrl) {
         const content = await this.loadContent(pageUrl);
         this.contentElement.innerHTML = content;
     }
 
+    // Lädt den Footer 
     async loadFooter() {
         const footerContent = await this.loadContent('footer.html');
         this.footerElement.innerHTML = footerContent;
     }
 }
 
-// Initialisierung und erste Ladung
+// Laden von header, footer und und als Standart die Hauptseite diese wird dann durch die Funktion 'loadPage' ersetzt
 const loader = new ContentLoader();
 document.addEventListener('DOMContentLoaded', async () => {
     await loader.loadPage('frontpage.html');
