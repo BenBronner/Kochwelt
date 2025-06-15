e5ac9d7ea8b57c585cacc9fe9d038cacb3ccd98c
Ben Bronner <crow@black-bird-design.de>
2025-06-15 15:52:51 +0200

    added Simple js for calculate Reziept

---
+++ scripts/calculateResieptSimple.js
@@ -0,0 +1,5 @@
+// Funktion zum Auslesen und Berechnen des Variblenwerts und der Eingabe in das Inputfeld nach Portionen mit document.getElementById
+function calculatePortion() {
+    document.getElementById("ingredients1").innerHTML = (600) / 4 * +itemamount.value;
+   
+};
