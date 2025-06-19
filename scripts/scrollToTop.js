b7ce045a5053f962c15f39b689e149b843bdd797
Ben Bronner <crow@black-bird-design.de>
2025-06-19 14:04:29 +0200

    added scrollToTop

---
+++ scripts/scrollToTop.js
@@ -0,0 +1,16 @@
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("backToTop").style.display = "block";
+    } else {
+        document.getElementById("backToTop").style.display = "none";
+    }
+}
